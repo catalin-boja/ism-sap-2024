48b22b53486132dc6ce90fb3f03872975fbfde0a
Catalin Boja <catalin.boja@ie.ase.ro>
2024-10-19 12:25:21 +0300

    day 1 update

---
+++ DAY01_Strings/src/ro/ase/ism/sap/TestStrings.java
@@ -0,0 +1,108 @@
+package ro.ase.ism.sap;
+
+public class TestStrings {
+
+	public static void main(String[] args) {
+		
+		String file1 = "Keys.txt";
+		String file2 = "Keys.txt";
+		
+		if(file1 == file2) {
+			System.out.println("The files are the same");
+		}
+		else {
+			System.out.println("The files are different");
+		}
+		
+		file2 = new String("Keys.txt");
+		if(file1 == file2) {
+			System.out.println("The files are the same");
+		}
+		else {
+			System.out.println("The files are different");
+		}
+		
+		if(file1.equals(file2)) {
+			System.out.println("The files are the same");
+		}
+		else {
+			System.out.println("The files are different");
+		}
+		
+		//small numbers from 0 to 127 are managed by a Int constant pool
+		int vb = 10;
+		
+//		Integer intVb = 10;
+//		Integer intVb2 = 10;
+		
+		Integer intVb = 128;
+		Integer intVb2 = 128;
+		
+		if(intVb == intVb2) {
+			System.out.println("The 2 numbers are the same");
+		} else {
+			System.out.println("The 2 numbers are different");
+		}
+		
+		if(intVb.equals(intVb2)) {
+			System.out.println("The 2 numbers are the same");
+		} else {
+			System.out.println("The 2 numbers are different");
+		}
+		
+		//converting numbers to strings
+		int value = 33;
+		String binaryRep = Integer.toBinaryString(value);
+		String hexRep = Integer.toHexString(value);
+		
+		System.out.println("Binary string: " + binaryRep);
+		System.out.println("Hex string: " + hexRep);
+
+		byte smallValue = 23;
+		System.out.println("Binary string: " +Integer.toBinaryString(smallValue));
+		System.out.println("Hex string: " + Integer.toHexString(smallValue));
+		
+
+		///from string to numbers
+		Integer initialValue = Integer.parseInt(hexRep,16);
+		System.out.println("Initial value is: " + initialValue);
+		initialValue = Integer.parseInt(binaryRep,2);
+		System.out.println("Initial value is: " + initialValue);
+		
+		
+		byte smallValue2 = -23;
+		System.out.println("Binary string: " +Integer.toBinaryString(smallValue2));
+		System.out.println("Hex string: " + Integer.toHexString(smallValue2));
+			
+		
+		System.out.println(	Byte.toUnsignedInt(smallValue2));
+		
+		
+		System.out.println(
+				"Binary string: " + 
+		Integer.toBinaryString(Byte.toUnsignedInt(smallValue2)));
+		System.out.println("Hex string: " + 
+		Integer.toHexString(Byte.toUnsignedInt(smallValue2)));
+		
+	
+		byte[] hash = {(byte)23, (byte)-23, (byte)10, (byte)5};
+		//wrong way
+		String hashHexString = "";
+		for(int i = 0; i < hash.length; i++) {
+			hashHexString += Integer.toHexString(hash[i]);
+		}
+		
+		System.out.println("The hash is " + hashHexString);
+		
+		
+		//byte[] hash = {(byte)23, (byte)-23, (byte)10, (byte)5};
+		//wrong way
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < hash.length; i++) {
+			sb.append(Integer.toHexString(Byte.toUnsignedInt(hash[i])));
+		}
+		System.out.println("The hash is " + sb.toString().toUpperCase());
+		
+	}
+
+}
